353fa60d82fbd504656555b13b6def6e606e3f41
Sergio Campos <morrodogg8@gmail.com>
2019-07-19 00:45:24 -0700

    finished

--- p3.10/main.cpp
+++ p3.10/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  p3.10
-//
-//  Created by Sergio campos on 7/19/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ • P3.10
+ Write a program that reads in the x- and y-coordinates of two corner points of a rectangle and then prints out whether the rectangle is a square, or is in “portrait” or “landscape” orientation.
+ */
 
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Enter first cordinates: ";
+    float x1, x2, y1, y2;
+    cin >> x1 >> y1;
+    cout << "Enter second cordinates: ";
+    cin >> x2 >> y2;
+    float height = abs(y1 + y2);
+    float length = abs(x1 + x2);
+    
+    if (height > length)
+    {
+        cout << "portrait";
+    }
+    else if (length > height)
+    {
+        cout << "landscape";
+    }
+    else
+    {
+        cout << "square";
+    }
+    cout << endl;
 }
